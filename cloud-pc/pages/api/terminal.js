9770a3573ded807d2779e81d6a81e42e0ea29610
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-09-25 00:19:12 +0000

    init

---
+++ cloud-pc/pages/api/terminal.js
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+    const output = "";
+    res.status(200).json({ output })
+}
+  
